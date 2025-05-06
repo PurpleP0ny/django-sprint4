bb1648d6781780f62b300586ef3cc7885e08ab20
PurpleP0ny <you@example.com>
2025-05-06 20:53:45 +0300

    A comment counter has been added and the comments model has been registered

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group, User
+from django.utils.html import format_html
 
-from .models import Category, Location, Post
+from .models import Category, Comment, Location, Post
 
 
 @admin.register(Category)
@@ -37,7 +38,28 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('is_published', 'category', 'author', 'location')
     search_fields = ('title', 'text')
     date_hierarchy = 'pub_date'
-    raw_id_fields = ('author',)
+    raw_id_fields = ('author', 'location', 'category')
+
+    @admin.display(description='Комментарии')
+    def comment_count(self, obj):
+        return obj.comments.count()
+
+    @admin.display(description='Изображение')
+    def display_name(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" width="100" height="100" />',
+                obj.image.url
+            )
+        return 'Изображение отсутсвует'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'author', 'post', 'created_at')
+    list_filter = ('author', 'post', 'created_at')
+    search_fields = ('text',)
+    raw_id_fields = ('author', 'post')
 
 
 class SimpleUserAdmin(UserAdmin):
