89a734c8ea1fdf600eb56d9f8a77762dfd6eab38
PurpleP0ny <you@example.com>
2025-05-06 21:06:51 +0300

    Improve forms: remove author field, fix datetime widget, add textarea

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -15,10 +15,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text', 'pub_date', 'location', 'category', 'image')
-        widgets = {
-            'pub_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-        }
+        exclude = ('author',)
 
 
 class CommentForm(forms.ModelForm):
@@ -26,3 +23,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
+        widgets = {
+            'text': forms.Textarea(attrs={'cols': 22, 'rows': 5})
+        }
